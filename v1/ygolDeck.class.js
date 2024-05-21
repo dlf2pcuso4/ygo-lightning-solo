a76042fb85370138b25d5894f4a06a2f6fd60b1e
Daniel Kwong <danielthkwong@gmail.com>
2024-05-22 00:17:12 +1000

    autoupdate

--- v1/ygolDeck.class.js
+++ v1/ygolDeck.class.js
@@ -112,7 +112,7 @@ class YgolDeck {
   namelist_to_ydk(namelist) {
     let arr = namelist.split("\n");
     for (let i = 0; i < arr.length; i++) {
-      let card = this.db.filter((a) => a.name == arr[i]);
+      let card = this.db.filter((a) => a.name == arr[i] && !a.rush);
       if (card.length) arr[i] = card[0].konamiID || arr[i];
     }
     return arr.join("\n");
